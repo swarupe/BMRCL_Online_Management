297cd2a5fbd3bfd6cdba1e54c1c49ec37543a280
Swaroop <32168917+swarupe@users.noreply.github.com>
2017-11-11 13:23:26 +0530

    Deleted some files and added bootstrap effects for some pages.

--- php/card_status_login.php
+++ php/card_status_login.php
@@ -5,29 +5,29 @@ include("connect.php");
 $con = OpenCon();
 
 
-  if(isset($_POST['submit']))
-  {
-  	$username = $_POST['username'];
-  	$password = $_POST['password'];
-	$status = mysqli_query($con,"SELECT Card_Status FROM smartcard S, customer C WHERE S.Cust_Id = C.Cust_Id AND Username = '$username' AND Password = '$password' ");
-	$row_cnt = mysqli_num_rows($status);
-	$row = $status->fetch_array();
-  }
+if(isset($_POST['submit']))
+{
+ $username = $_POST['username'];
+ $password = $_POST['password'];
+ $status = mysqli_query($con,"SELECT S.Card_Status FROM smartcard S, customer C WHERE S.Cust_Id = C.Cust_Id AND C.Username = '$username' AND C.Password = '$password' ");
+ $row_cnt = mysqli_num_rows($status);
+ $row = $status->fetch_array();
+}
 
-   if($row_cnt==1)
-   {
-   		session_start();
+if($row_cnt > 0)
+{
+ session_start();
 
-        $_SESSION['username'] = $username;
-        $_SESSION['Card_Status'] = $row;
-        header("Location: ../HomePage.html");
-    }
-    else {
+ $_SESSION['username'] = $username;
+ $_SESSION['Card_Status'] = $row;
+ header("Location: ../HomePage.html");
+}
+else {
 
-    	$_SESSION['error_message']="Wrong Username or Password";
-        header("location: ../smart_card_request_page.php");
-    }
+ $_SESSION['error_message']="Wrong Username or Password";
+ header("location: ../smart_card_login.php");
+}
 
-        Closecon($con);
+Closecon($con);
 
 ?>
